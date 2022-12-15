8764d0fad6c581112ea3278cc5ba1a06b960fd63
johnswaroop <71548636+johnswaroop@users.noreply.github.com>
2022-12-15 21:31:24 +0530

    updates

--- pages/api/genHash.js
+++ pages/api/genHash.js
@@ -3,7 +3,6 @@ import { ethers } from "ethers"
 
 let msg = "sample"
 
-
 export default function handler(req, res) {
     const msgHash = ethers.utils.hashMessage(msg);
     res.status(200).json({ hash: msgHash });
